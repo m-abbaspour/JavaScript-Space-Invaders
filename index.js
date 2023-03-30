39bf2acee383d07a2e3cb214fbc29ff2a1f4e6c6
Maddie Abbaspour <89539144+m-abbaspour@users.noreply.github.com>
2023-03-29 22:21:04 -0500

    create projectiles and an invader

--- index.js
+++ index.js
@@ -62,7 +62,73 @@ class Player {
     }
 }
 
+class Invader {
+    constructor() {
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = './img/invader.png'
+        image.onload = () => {
+            const scale = 1
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height / 2
+            }
+        }
+    }
+
+    draw() {
+        c.drawImage(
+            this.image, 
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height
+        )
+    }
+
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+            this.position.y += this.velocity.y
+        }
+    }
+}
+
+class Projectile {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+
+        this.radius = 3
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2)
+        c.fillStyle = 'red'
+        c.fill()
+        c.closePath()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
 const player = new Player()
+const projectiles = []
+const invader =  new Invader()
 const keys = {
     a: {
         pressed: false
@@ -79,7 +145,17 @@ function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    invader.update()
     player.update()
+    projectiles.forEach((projectile, index) => {
+        if(projectile.position.y+projectile.radius <= 0) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            })
+        } else {
+            projectile.update()
+        }
+    })
 
     if (keys.a.pressed && player.position.x >= 0) {
         player.velocity.x = -7
@@ -98,15 +174,26 @@ animate()
 window.addEventListener('keydown', ({key}) => {
     switch (key) {
         case 'a':
-            console.log('left')
+            // console.log('left')
             keys.a.pressed = true
             break;
         case 'd':
-            console.log('right')
+            // console.log('right')
             keys.d.pressed = true
             break;
         case ' ':
-            console.log('space')
+            // console.log('space')
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x + player.width / 2,
+                    y: player.position.y 
+                }, 
+                velocity: {
+                    x: 0,
+                    y:-10
+                }
+            }))
+            // console.log(projectiles)
             break;
     }
 })
@@ -114,15 +201,15 @@ window.addEventListener('keydown', ({key}) => {
 window.addEventListener('keyup', ({key}) => {
     switch (key) {
         case 'a':
-            console.log('left')
+            // console.log('left')
             keys.a.pressed = false
             break;
         case 'd':
-            console.log('right')
+            // console.log('right')
             keys.d.pressed = false
             break;
         case ' ':
-            console.log('space')
+            // console.log('space')
             break;
     }
 })
